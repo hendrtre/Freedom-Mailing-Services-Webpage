1ab6d180608afb62964dbd5cd82a071f0163954b
Trent Hendrickson <trentdev2020@gmail.com>
2020-03-17 16:31:54 -0600

    added '.vscode' file and json settings to make things easier on myself. Plus I changed the title in the 'app.js' file of components.

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>Welcome to Freedom Mailing Services!</h1>
       </div>
     );
   }
