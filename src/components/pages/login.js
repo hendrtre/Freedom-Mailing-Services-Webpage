6d8b1d5ec0a21021a734e1b58b6d5209cb9657e3
Trent Hendrickson <trentdev2020@gmail.com>
2020-03-25 14:01:13 -0600

    created a form for email and password

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react'
 
 export default class login extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            email: "",
+            password: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(event) {
+        console.log("Handle submit", event)
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
 
                 <form>
-                    <input type="text"/>
-                    <input type="password"/>
+                    <input onSubmit={this.handleSubmit}
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+
+                    <input 
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
                 </form>
             </div>
         )
