76298a788a9db49f68d112e8d2252904b3b2aa3f
Trent Hendrickson <trentdev2020@gmail.com>
2020-03-30 12:00:58 -0600

    Added a lot of styling to the contact page.

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -6,23 +6,40 @@ export default class Contact extends Component {
       <div className="contact-page-wrapper">
         <div className="far-left-whitespace"></div>
         <div className="maroon-ribbon"></div>
-        <div className="form-container">
-          <form>
-            <input 
-              type="text" 
-              name="fullName" 
-              placeholder="Your Name" 
-            />
 
-            <input 
-              type="email"
-              name="email"
-              placeholder="Email"
-            />
-            
-          </form>
+        <div className="right-side">
+          <h1>For further information or questions, contact us at:</h1>
+
+          <div className="form-container">
+            <form action="" className="form">
+              <div className="name-email-wrapper">
+                <div className="form-group-fullName">
+                  <input 
+                    type="text" 
+                    name="fullName" 
+                    placeholder="Name" 
+                  />
+                  <label htmlFor="name">Your Name</label>
+                </div>
+
+                <div className="form-group">
+                  <input 
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                  />
+                  <label htmlFor="email">Email</label>
+                </div>
+              </div>
+
+              <div className="form-group">
+                <textarea name="message" id="message" rows="7" placeholder="Description"></textarea>
+              </div>
+
+              <button type="submit" className="btn-send">Send</button>
+            </form>
+          </div>
         </div>
-
       </div>
     );
   }
